4318ab9223e76742d3ed128d0b022445126761fd
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-03-28 19:29:31 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,21 +5,34 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     loop = !(loop)
-    n1 = 1
-    n2 = 1
+    fib = 1
+    n = 0
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showIcon(IconNames.Asleep)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        . # # # .
+        . . # . .
+        . . # . .
+        `)
     basic.showString(receivedString)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        . # # # .
+        . . # . .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
     radio.sendString("Bla!")
     basic.showIcon(IconNames.Yes)
 })
+let tmp = 0
+let n = 0
 let fib = 0
-let n2 = 0
-let n1 = 0
 let loop = false
 radio.setGroup(255)
 basic.showIcon(IconNames.Happy)
@@ -29,10 +42,10 @@ basic.forever(function () {
         basic.showIcon(IconNames.Heart)
         basic.showIcon(IconNames.SmallHeart)
     } else {
-        basic.showString("" + (n1))
-        fib = n1 + n2
-        n1 = n2
-        n2 = fib
+        basic.showString("" + (fib))
+        tmp = fib
+        fib = fib + n
+        n = tmp
         basic.pause(1500)
         basic.clearScreen()
         basic.pause(500)
