ac900fffe0146dff80f99592285647c45f8670bf
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-03-28 15:09:28 +0200

    Implement radio

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.No)
+    radio.sendString("Boe!")
+    basic.showIcon(IconNames.Yes)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(255)
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
 })
