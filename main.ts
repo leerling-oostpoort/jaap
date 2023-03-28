c5e16f7f8092a168ff4e283090e1c6e3b0644604
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-03-28 15:55:53 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
 })
 radio.onReceivedString(function (receivedString) {
+    basic.showIcon(IconNames.Asleep)
     basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    radio.sendString("Bla!")
+    basic.showIcon(IconNames.Yes)
 })
 radio.setGroup(255)
 basic.showIcon(IconNames.Happy)
