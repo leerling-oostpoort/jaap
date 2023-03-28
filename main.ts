d44eb0078e58cb1c84c432129f7c7c32191c5d55
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-03-28 16:48:29 +0200

    Add fibonacci sequence display

--- main.ts
+++ main.ts
@@ -3,6 +3,11 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("Boe!")
     basic.showIcon(IconNames.Yes)
 })
+input.onButtonPressed(Button.AB, function () {
+    loop = !(loop)
+    n1 = 1
+    n2 = 1
+})
 radio.onReceivedString(function (receivedString) {
     basic.showIcon(IconNames.Asleep)
     basic.showString(receivedString)
@@ -12,9 +17,24 @@ input.onButtonPressed(Button.B, function () {
     radio.sendString("Bla!")
     basic.showIcon(IconNames.Yes)
 })
+let fib = 0
+let n2 = 0
+let n1 = 0
+let loop = false
 radio.setGroup(255)
 basic.showIcon(IconNames.Happy)
+basic.pause(2000)
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
-    basic.showIcon(IconNames.SmallHeart)
+    if (!(loop)) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    } else {
+        basic.showString("" + (n1))
+        fib = n1 + n2
+        n1 = n2
+        n2 = fib
+        basic.pause(1500)
+        basic.clearScreen()
+        basic.pause(500)
+    }
 })
