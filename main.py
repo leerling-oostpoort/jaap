4f09a815bbacbc835cbffa859cb4ccd1afcbda98
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-04-02 22:08:28 +0200

    Use timer instead of "forever", improve heart animation, use buttons for fibonacci

--- main.py
+++ main.py
@@ -1,3 +1,74 @@
+def on_button_pressed_a():
+    basic.show_icon(IconNames.NO)
+    radio.send_string("Boe!")
+    basic.show_icon(IconNames.YES)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global state
+    state += 1
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_received_string(receivedString):
+    basic.show_leds("""
+        # # # # #
+                # . # . #
+                . # # # .
+                . . # . .
+                . . # . .
+    """)
+    basic.show_string(receivedString)
+    basic.show_leds("""
+        # # # # #
+                # . # . #
+                . # # # .
+                . . # . .
+                . . # . .
+    """)
+radio.on_received_string(on_received_string)
+
+def on_button_pressed_b():
+    basic.show_icon(IconNames.NO)
+    radio.send_string("Bla!")
+    basic.show_icon(IconNames.YES)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+radio.set_group(255)
+basic.show_icon(IconNames.HAPPY)
+basic.pause(2000)
+for index in range(4):
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+    """)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    """)
+states = 2
+state = 0
+
 def on_forever():
-    pass
+    if state % states == 0:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        . . . . .
+        """)
+        basic.show_icon(IconNames.SMALL_HEART)
+        basic.show_icon(IconNames.HEART)
+        basic.pause(200)
+        basic.show_icon(IconNames.SMALL_HEART)
+    elif state == 1:
+        pass
+    else:
+        pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,14 +1,67 @@
-input.onButtonPressed(Button.A, function () {
+function nextLed () {
+    if (led_state == 0) {
+        basic.clearScreen()
+    } else if (led_state == 2) {
+        dot.showImage(0)
+    } else if (led_state == 3) {
+        images.iconImage(IconNames.SmallHeart).showImage(0)
+    } else if (led_state == 4) {
+        images.iconImage(IconNames.Heart).showImage(0)
+    } else if (led_state == 7) {
+        images.iconImage(IconNames.SmallHeart).showImage(0)
+    } else if (led_state == 8) {
+        dot.showImage(0)
+    }
+    led_state += 1
+    if (led_state > 8) {
+        led_state = 0
+    }
+}
+function sendBar () {
     basic.showIcon(IconNames.No)
-    radio.sendString("Boe!")
+    radio.sendString("Bar")
     basic.showIcon(IconNames.Yes)
+}
+function prevFib () {
+    tmp = fibn
+    fibn = fib - fibn
+    fib = tmp
+}
+input.onButtonPressed(Button.A, function () {
+    if (state == 0) {
+        sendFoo()
+    } else if (state == 1) {
+        prevFib()
+    }
 })
+function showInit () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(2000)
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+}
 input.onButtonPressed(Button.AB, function () {
-    loop = !(loop)
-    fib = 1
-    n = 0
+    state += 1
+    if (state >= states) {
+        state = 0
+    }
 })
 radio.onReceivedString(function (receivedString) {
+    busy = true
     basic.showLeds(`
         # # # # #
         # . # . #
@@ -26,28 +79,54 @@ radio.onReceivedString(function (receivedString) {
         `)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No)
-    radio.sendString("Bla!")
-    basic.showIcon(IconNames.Yes)
+    if (state == 0) {
+        sendBar()
+    } else if (state == 1) {
+        nextFib()
+    }
 })
+function showFib () {
+    basic.showString("" + (fib))
+}
+function sendFoo () {
+    basic.showIcon(IconNames.No)
+    radio.sendString("Foo")
+    basic.showIcon(IconNames.Yes)
+}
+function nextFib () {
+    tmp = fib
+    fib = fib + fibn
+    fibn = tmp
+}
 let tmp = 0
-let n = 0
+let busy = false
+let dot: Image = null
+let fibn = 0
 let fib = 0
-let loop = false
+let led_state = 0
+let state = 0
+let states = 0
 radio.setGroup(255)
-basic.showIcon(IconNames.Happy)
-basic.pause(2000)
-basic.forever(function () {
-    if (!(loop)) {
-        basic.showIcon(IconNames.Heart)
-        basic.showIcon(IconNames.SmallHeart)
-    } else {
-        basic.showString("" + (fib))
-        tmp = fib
-        fib = fib + n
-        n = tmp
-        basic.pause(1500)
-        basic.clearScreen()
-        basic.pause(500)
+states = 2
+state = 0
+led_state = 0
+fib = 1
+fibn = 0
+dot = images.createImage(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+busy = false
+showInit()
+loops.everyInterval(100, function () {
+    if (!(busy)) {
+        if (state == 0) {
+            nextLed()
+        } else if (state == 1) {
+            showFib()
+        }
     }
 })
